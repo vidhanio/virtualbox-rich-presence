8f3289d5ee3d93719e53553d983c86c4cea57050
kreyoo <zunder325@gmail.com>
2021-01-07 16:08:07 +0100

    Reworked installation process

---
+++ prepare.py
@@ -0,0 +1,26 @@
+# This installs the required Python-Modules dependant on the used platform
+import pip, sys
+
+
+
+def install(packages):
+    for package in packages:
+        pip.main(['install', package])
+
+all_platforms = [
+    "pyvbox",
+    "pypresence",
+    "psutil",
+    "virtualbox",
+    "pypresence",
+    "vboxapi",
+]
+
+windows = [
+    "pywin32"
+]
+
+install(all_platforms)
+
+if sys.platform == "win32":
+    install(windows)
